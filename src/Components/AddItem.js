3d568ae2e66402c1a8f63ae5c60d4ee3dbd6411c
Mugil1410 <mugilaananthan@gmail.com>
2024-04-28 17:48:53 +0530

    first commit

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const AddItem = ({ NewItem, setNewItem, handleSubmit }) => {
   return (
-    <div>
-      <h1>Add Item</h1>
+    <div style={{}}>
+      <h2>What is today’s plan</h2>
       <form className="addForm" onSubmit={handleSubmit}>
         <label htmlFor="additem">Add new task</label>
         <input
           type="text"
           autoFocus
           id="additem"
-          placeholder="Add Item"
+          placeholder="Add Task"
           required
           value={NewItem}
           onChange={(e) => setNewItem(e.target.value)}
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,14 @@
 
 const Footer = ({length,Completed,Incompleted}) => {
   return (
-    
     <footer>
       <table>
       <th>{length ===0 ? null:"Tasks: "+length+" |"}</th>
-      <th>{Completed ===0 ? null:" Completed: "+Completed+" |"}</th>
-      <th>{Incompleted ===0 ? null:" Incompleted: "+Incompleted}</th>
+      <th>{Completed ===0 ? null:"Completed: "+Completed+" |"}</th>
+      <th>{Incompleted ===0 ? null:"Incompleted: "+Incompleted}</th>
       </table>
       <br />
-      <p >created by <a href="https://mugil.netlify.app/" style={{color: "inherit"}}>Mugilaananthan</a></p>
+      <center>Crafted By <a style={{color: "inherit",textDecoration:"none"}} href="https://mugil.netlify.app/">Mugilaananthan</a></center>
     </footer>
   )
 }
